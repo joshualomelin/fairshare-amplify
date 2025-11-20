864e9bccebbe4adcd216d0931e223c0c25d4342f
Joshua Lomelin <joshualomelin@gmail.com>
2025-11-20 23:28:09 +0000

    Adjusted import for amplify_outputs.json

--- src/HouseholdBillApp.jsx
+++ src/HouseholdBillApp.jsx
@@ -7,7 +7,7 @@ import { DollarSign, Plus, LogOut, Home, Users, CreditCard, CheckCircle, Loader2
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from './amplify_outputs.json'; 
+import outputs from '../amplify_outputs.json'; 
 
 Amplify.configure(outputs);
 
